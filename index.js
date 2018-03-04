d27a316e537aea1ae0d185c3257d36d6bca4efda
lucas pereira da cunha <lucaspereiracunha08@gmail.com>
2018-03-04 11:22:45 -0800

    flickity and text

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-var timer = setInterval(function(){
-    console.log(new date()); 
-}, 1000);
+
+
+// $(function() {
+//     var width = 320;
+//     var currentSlide = 1;
+
+//     var end = $('.sales').find('ul');
+//     setInterval (function() {
+//         $('.sales ul').animate({'margin-left': '-='+width}, 1000);
+//         currentSlide = currentSlide + 1;
+//         if (currentSlide === $('.sales li').length) {
+//             currentSlide = 1;
+//             $(end).css ('margin-left', 0);
+//         }
+//     }, 3000);
+// });
