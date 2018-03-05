417a35e323aebb98e5ac313bc4a58a967ed17533
lucas pereira da cunha <lucaspereiracunha08@gmail.com>
2018-03-05 15:08:40 -0800

    test

--- index.js
+++ index.js
@@ -12,26 +12,25 @@ $('.clic-nav').ready(function() {
 
 // FLickity automatically arranging the sales bar
 
+$('.subscribe').ready(function(email_c) {
+    $('.s_button').click(function() {
+    var email_c = $('button_t').val();
+    if (validateEmail(email_c)) {
+    alert('Thanks for subscribing!');
+    }
+    else {
+    alert('Please Enter A Valid E-Mail');
+    email_c.preventDefault();
+    }
+    });
+});
 
-// $('.subscribe').ready(function(email_c) {
-//     $('.s_button').click(function() {
-//     var email_c = $('button_t').val();
-//     if (validateEmail(email_c)) {
-//     alert('Thanks for subscribing!');
-//     }
-//     else {
-//     alert('Please Enter A Valid E-Mail');
-//     e.preventDefault();
-//     }
-//     });
-// });
-
-// function validateEmail(email_c) {
-//     var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
-//     if (filter.test(email_c)) {
-//     return true;
-//     }
-//     else {
-//     return false;
-//     }
-//     }
+function validateEmail(email_c) {
+    var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+    if (filter.test(email_c)) {
+    return true;
+    }
+    else {
+    return false;
+    }
+    }
