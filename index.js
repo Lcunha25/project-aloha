35cee9632312f3079621b61dc9aec3a9d4a25a79
lucas pereira da cunha <lucaspereiracunha08@gmail.com>
2018-03-04 20:36:38 -0800

    scroll bar working

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
+$(document).ready(function() {
 
+    $('.slide_section').click(function(event){
+        var linkHref=$(this).attr('href');
+        console.log(linkHref);
+    $('html, body').animate({
+        scrollTop: $(linkHref).offset().top
+    });
+    event.preventDefault();
+    });
+});
 
-// $(function() {
-//     var width = 320;
-//     var currentSlide = 1;
-
-//     var end = $('.sales').find('ul');
-//     setInterval (function() {
-//         $('.sales ul').animate({'margin-left': '-='+width}, 1000);
-//         currentSlide = currentSlide + 1;
-//         if (currentSlide === $('.sales li').length) {
-//             currentSlide = 1;
-//             $(end).css ('margin-left', 0);
-//         }
-//     }, 3000);
-// });
+// FLickity automatically arranging the sales bar
