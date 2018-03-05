abaa73ee1a22e36839eef42a9fbacd916d49bc30
lucas pereira da cunha <lucaspereiracunha08@gmail.com>
2018-03-05 03:06:26 -0800

    Adjusting nav bar for mobile

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$('.clic-nav').ready(function() {
 
     $('.slide_section').click(function(event){
         var linkHref=$(this).attr('href');
@@ -11,3 +11,27 @@ $(document).ready(function() {
 });
 
 // FLickity automatically arranging the sales bar
+
+
+// $('.subscribe').ready(function(email_c) {
+//     $('.s_button').click(function() {
+//     var email_c = $('button_t').val();
+//     if (validateEmail(email_c)) {
+//     alert('Thanks for subscribing!');
+//     }
+//     else {
+//     alert('Please Enter A Valid E-Mail');
+//     e.preventDefault();
+//     }
+//     });
+// });
+
+// function validateEmail(email_c) {
+//     var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+//     if (filter.test(email_c)) {
+//     return true;
+//     }
+//     else {
+//     return false;
+//     }
+//     }
